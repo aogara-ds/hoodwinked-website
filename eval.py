51763c9f58f3987c3295828b193d3f614a8e0e37
Aidan O'Gara <aidanogara623@gmail.com>
2022-10-27 13:59:14 -0700

    Trim prompts to max GPT-3 context length

--- eval.py
+++ eval.py
@@ -6,28 +6,39 @@ from src.agent import Player
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+def get_save_path():
+    """
+    Returns a pathname to be used throughout the evaluation. 
+    """
+    save_dir = 'results'
+    file_name = str(len([name for name in os.listdir(save_dir)
+                    if os.path.isfile(os.path.join(save_dir, name))]))
+    full_path = save_dir + '/' + file_name + '.csv'
+    return full_path
+
 # Prepare DataFrame to store results
 columns = ['Game Number', "Setup", 'Runtime',
            'Number of Players', 'Discussion']
 eval_df = pd.DataFrame(columns=columns)
+save_path = get_save_path()
 
 # Run a number of games
-for game_num in range(20):
+for game_num in range(1000):
     # Time the game
     start_time = time.time()
 
     # Define the game
-    discussion = False
+    discussion = (game_num % 2 == 0)
     game = Game(discussion=discussion)
 
     # Load the players into the game
     game.load_players([
-        Player("Regan", killer=False, agent="random"),
-        Player("Amy", killer=True, agent="random"),
-        Player("Spencer", killer=False, agent="random"),
-        Player("Lena", killer=False, agent="random"),
-        Player("Tim", killer=False, agent="random"),
-        Player("Bob", killer=False, agent="random")
+        Player("Regan", killer=False, agent="gpt3"),
+        Player("Amy", killer=True, agent="gpt3"),
+        Player("Spencer", killer=False, agent="gpt3"),
+        Player("Lena", killer=False, agent="gpt3"),
+        Player("Tim", killer=False, agent="gpt3"),
+        Player("Bob", killer=False, agent="gpt3")
     ])
     # Play the game
     player_dicts = game.play()
@@ -110,10 +121,10 @@ for game_num in range(20):
     eval_df.loc[game_idxs, "Number of Players"] = len(game.players)
     eval_df.loc[game_idxs, "Discussion"] = discussion
 
+    # Save results every 50 games
+    if game_num % 50 == 0:
+        print(f"Saving first {x} games")
+        eval_df.to_csv(save_path)
 
 # Save results as CSV
-save_dir = 'results'
-file_name = str(len([name for name in os.listdir(save_dir)
-                if os.path.isfile(os.path.join(save_dir, name))]))
-full_path = save_dir + '/' + file_name + '.csv'
-eval_df.to_csv(full_path)
+eval_df.to_csv(save_path)
--- src/gpt3.py
+++ src/gpt3.py
@@ -5,23 +5,32 @@ from transformers import GPT2Tokenizer
 
 
 class GPT3():
-    def __init__(self):
+    def __init__(self, max_tokens = 16, temperature = 1):
         print("Configuring GPT-3")
         load_dotenv()
         openai.api_key = os.getenv('OPENAI_API_KEY')
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
+        # Set hyperparameters
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+
     def tokenize(self, prompt):
         return self.tokenizer(prompt)['input_ids']
 
     def generate(self, prompt, max_tokens):
+        # Ensure prompt is below 1024 tokens
+        prompt = self.trim_prompt(prompt)
 
+        # Fetch response from OpenAI API
         response = openai.Completion.create(
             model="text-davinci-002",
             prompt=self.tokenize(prompt),
             temperature=0.8,
             max_tokens=max_tokens
         )
+
+        # Extract and clean generated text response
         response = response['choices'][0]['text']
         response = response.replace('\n', '')
         return response
@@ -36,3 +45,21 @@ class GPT3():
         )
         logprobs = logprobs['choices'][0]['logprobs']['top_logprobs'][0]
         return logprobs
+    
+    def trim_prompt(self, prompt):
+        # While the prompt is too long, delete turns
+        delete_turn_num = 0
+        while len(self.tokenize(prompt) > 1024 - self.max_tokens):
+            # Identify the beginning and end position of the target turn
+            delete_turn_num += 1
+            start_pos = prompt.find(f"Turn #{delete_turn_num}")
+            end_pos = prompt.find(f"Turn #{delete_turn_num + 1}")
+            prompt = prompt[:start_pos] + "...\n\n" + prompt[end_pos:]
+
+        # Remove excess space from prompt
+        excess = "...\n\n...\n\n"
+        while excess in prompt:
+            prompt=prompt.replace(excess,"...\n\n")
+        
+        return prompt
+    
