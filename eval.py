334f2de5d359ff8dcfda5cc280d8888a0fc51e6a
Aidan O'Gara <aidanogara623@gmail.com>
2022-11-02 10:24:38 -0700

    batch scheduling of evaluation jobs

--- eval.py
+++ eval.py
@@ -6,6 +6,80 @@ from src.agent import Player
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+def run_job(
+    num_games: int, 
+    num_players: int, 
+    impostor_agent: str, 
+    innocent_agent: str, 
+    discussion: bool, 
+    start_location: str,
+    eval_cols: list[str],
+):
+    """
+    Runs a number of games with the given specifications. 
+    Returns a dictionary of eval results with a row for each player. 
+    """
+    # Initialize dictionary to be returned
+    eval_dict = {colname: [] for colname in eval_cols}
+
+    # Run each game and store results
+    for i in range(1, num_games+1):
+        try:
+            # Time the game
+            start_time = time.time()
+
+            # Define the game
+            discussion = discussion
+            game = Game(discussion=discussion, start_location=start_location)
+
+            # Load the players into the game
+            game.load_random_players(num_players, impostor_agent, innocent_agent)
+
+            # Play the game
+            player_dicts = game.play()
+
+            # Record the runtime
+            end_time = time.time()
+            runtime = end_time - start_time
+
+            # Condense player dicts into a single dictionary
+            for player_dict in player_dicts:
+                for k in list(eval_dict.keys())[4:]:
+                    if k in player_dict.keys():
+                        eval_dict[k].append(player_dict[k])
+                    else:
+                        eval_dict[k].append("")
+            
+            # Store game-level information in eval_dict
+            # Duplicated in each agent's row for ease of display
+            # Memory inefficient -- change this if low on memory
+            eval_dict["game_num"].extend([i for _ in range(num_players)])
+            eval_dict["runtime"].extend([runtime for _ in range(num_players)])
+            eval_dict["num_players"].extend([num_players for _ in range(num_players)])
+            eval_dict["discussion"].extend([discussion for _ in range(num_players)])
+
+            # # Rate Limiting: Only three games per minute
+            api_hits = sum(eval_dict['num_turns'][-num_players:]) + \
+                sum([i if (type(i)!=str) else 0 for i in eval_dict['num_killed'][-num_players:]]) * 2 * num_players
+            print(f'api_hits {api_hits}')
+            # if time.time() - start_time < api_hits - 10:
+            #     sleeping_time =  api_hits - runtime + 10
+            #     print(f'sleep for {sleeping_time}')
+            #     time.sleep(sleeping_time)
+
+        # Catch errors and continue with the next game
+        except Exception as e:
+            print(e)
+            temp_save = pd.DataFrame(eval_dict)
+            temp_save_path = get_save_path().replace(".csv", "_temp.csv")
+            temp_save.to_csv(temp_save_path)
+            print("sleeping for 60")
+            time.sleep(60)
+            continue
+
+    return eval_dict
+
+
 def get_save_path():
     """
     Returns a pathname to be used throughout the evaluation. 
@@ -16,50 +90,42 @@ def get_save_path():
     full_path = save_dir + '/' + file_name + '.csv'
     return full_path
 
-# Prepare DataFrame to store results
-columns = ['Game Number', "Setup", 'Runtime',
-           'Number of Players', 'Discussion']
-eval_df = pd.DataFrame(columns=columns)
-save_path = get_save_path()
+if __name__ == "__main__":
+    # Read the schedule of jobs
+    schedule = pd.read_csv("jobs/schedule_8.csv")
+    save_path = get_save_path()
+    
+    # Set up the evaluation structure
+    results_cols = [
+        "game_num", "runtime", "num_players", "discussion",
+        "name", "agent", "killer", "num_turns", "banished",
+        "killed", "escaped", "num_killed", "num_escaped", "story"
+    ]
+    results = {colname: [] for colname in results_cols}
 
-# Run a number of games
-for game_num in range(1000):
-    # Time the game
-    start_time = time.time()
+    # Run each job individually
+    for idx, row in schedule.iterrows():
+        eval_dict = run_job(
+            num_games = row['num_games'],
+            num_players = row['num_players'],
+            impostor_agent = row['impostor_agent'],
+            innocent_agent = row['innocent_agent'],
+            discussion = row['discussion'],
+            start_location = row['start_location'],
+            eval_cols = results_cols
+        )
 
-    # Define the game
-    discussion = (game_num % 2 == 0)
-    game = Game(discussion=discussion)
+        # Join all eval dicts into a single results dict
+        for k, v in eval_dict.items():
+            if k not in results.keys():
+                results[k] = v
+            else:
+                results[k].extend(v)
+        
+        # Save results as .csv in results folder after each job
+        results_df = pd.DataFrame(results)
+        results_df.to_csv(save_path)
 
-    # Load the players into the game
-    game.load_players([
-        Player("Regan", killer=False, agent="gpt3"),
-        Player("Amy", killer=True, agent="gpt3"),
-        Player("Spencer", killer=False, agent="gpt3"),
-        Player("Lena", killer=False, agent="gpt3"),
-        Player("Tim", killer=False, agent="gpt3"),
-        Player("Bob", killer=False, agent="gpt3")
-    ])
-    # Play the game
-    player_dicts = game.play()
-    end_time = time.time()
 
-    # Store player results
-    eval_df = eval_df.append(player_dicts, ignore_index=True)
-
-    # Store game-level information
-    game_idxs = eval_df.loc[eval_df.shape[0]-len(game.players):
-                            eval_df.shape[0]-1].index
-    eval_df.loc[game_idxs, "Game Number"] = game_num
-    eval_df.loc[game_idxs, "Setup"] = "all_gpt"
-    eval_df.loc[game_idxs, "Runtime"] = end_time - start_time
-    eval_df.loc[game_idxs, "Number of Players"] = len(game.players)
-    eval_df.loc[game_idxs, "Discussion"] = discussion
-
-    # Save results every 50 games
-    if game_num % 20 == 0:
-        print(f"Saving first {game_num} games")
-        eval_df.to_csv(save_path)
-
-# Save results as CSV
-eval_df.to_csv(save_path)
+    # TODO: Reindex games
+    
