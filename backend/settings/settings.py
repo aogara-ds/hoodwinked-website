c87bf6fb88fcda15775f794e7219314c780242e3
Aidan O'Gara <aidanogara623@gmail.com>
2023-03-10 00:06:31 -0800

    Update allowed hosts

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -26,9 +26,11 @@ load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hoodwinked.onrender.com'
+]
 
 
 # Application definition
