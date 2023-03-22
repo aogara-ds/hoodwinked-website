5b6eb29b3a694e87bf2faf4aa0285b5f259ba8b0
Aidan O'Gara <aidanogara623@gmail.com>
2023-03-21 20:11:45 -0700

    update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -3,6 +3,6 @@ set -e # exit on error
 
 pip3 install -r requirements.txt
 
-python manage.py makemigrations --no-input
+python manage.py makemigrations hoodwinked
 
 python manage.py migrate --no-input
